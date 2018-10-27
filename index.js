c48480f997e90a4db6a8612a6c7ac4dd373bf989
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-10-28 00:35:55 +0200

    Add Preparations Table

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const restaurantsTable = require("./fake-data-tables/restaurants.table");
 const ordersTable = require("./fake-data-tables/orders.table");
 const mealsTable = require("./fake-data-tables/meals.table");
 const productsTable = require("./fake-data-tables/products.table");
+const preparationsTable = require("./fake-data-tables/preparations.table");
 
 require("./utils").writeToCSV(restaurantsTable.restaurants, restaurantsTable.csvPath); // Restaurants
 require("./utils").writeToCSV(ordersTable.orders, ordersTable.csvPath); // Orders
 require("./utils").writeToCSV(mealsTable.meals, mealsTable.csvPath); // Meals
 require("./utils").writeToCSV(productsTable.products, productsTable.csvPath); // Products
+require("./utils").writeToCSV(preparationsTable.preparations, preparationsTable.csvPath); // Preparations
