a0b4898a5a0d2055622d8d6ef03e8a686e7eca12
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-10-27 19:50:05 +0200

    Add generating primary key for restaurant

--- fake-data-tables/restaurants.table.js
+++ fake-data-tables/restaurants.table.js
@@ -3,19 +3,19 @@ const RESTARANTS_AMOUNT = 2;
 
 faker.locale = "pl";
 
-function randRestaurantData() {
+function randRestaurantData(id) {
   const randomRestaurant = faker.helpers.createCard(); // random contact card containing many properties
   const city = randomRestaurant.address.city;
   const street = randomRestaurant.address.streetC;
   const zipcode = randomRestaurant.address.zipcode;
-  return [city, street, zipcode];
+  return [id, city, street, zipcode];
 }
 
 function getRestaurants() {
   const restarantsArray = [];
-  restarantsArray.push(["Misto", "Adres", "Kod pocztowy"])
-  for (var i = 0; i < RESTARANTS_AMOUNT; i++) {
-    const restaurant = randRestaurantData();
+  restarantsArray.push(["id", "Misto", "Adres", "Kod pocztowy"])
+  for (var i = 1; i <= RESTARANTS_AMOUNT; i++) {
+    const restaurant = randRestaurantData(i);
     restarantsArray.push(restaurant);
   }
   return restarantsArray;
