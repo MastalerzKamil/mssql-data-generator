078242f6db561e9778e680e944870078841eb34b
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-10-27 23:09:48 +0200

    Add Products Table

---
+++ fake-data-tables/products.table.js
@@ -0,0 +1,16 @@
+module.exports = {
+  products: [
+    ["Produkty"],
+    ["frytki"],
+    ["salata"],
+    ["ketchup"],
+    ["bulka"],
+    ["cola"],
+    ["woda"],
+    ["poledwiczki"],
+    ["panierka"],
+    ["skrzydelka"],
+    ["udko"],
+    ["ser"]],
+  csvPath: "./generated-data/products.csv"
+}
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const restaurantsTable = require("./fake-data-tables/restaurants.table");
 const ordersTable = require("./fake-data-tables/orders.table");
 const mealsTable = require("./fake-data-tables/meals.table");
+const productsTable = require("./fake-data-tables/products.table");
 
 require("./utils").writeToCSV(restaurantsTable.restaurants, restaurantsTable.csvPath); // Restaurants
 require("./utils").writeToCSV(ordersTable.orders, ordersTable.csvPath); // Orders
-require("./utils").writeToCSV(mealsTable.meals, mealsTable.csvPath); // Orders
+require("./utils").writeToCSV(mealsTable.meals, mealsTable.csvPath); // Meals
+require("./utils").writeToCSV(productsTable.products, productsTable.csvPath); // Products
