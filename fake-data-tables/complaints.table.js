cc2f39cd672feec83788156807c64da4064c13b5
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-10-28 01:05:11 +0200

    Add Complaints table

---
+++ fake-data-tables/complaints.table.js
@@ -0,0 +1,32 @@
+const orders = require("./orders.table").orders;
+const utils = require("./../utils");
+
+const COMPLAINTS_AMOUNT = 1000;
+const complainReasons = ['Zbyt dlugi czas oczekiwania', 'nieswieze jedzenie', 'zle zapakowane jedzenie',
+  'inne'];
+
+function randComplaint(complaintId) {
+  const complaintIndex = utils.randomIntFromInterval(0, complainReasons.length - 1);
+  const reason = complainReasons[complaintIndex];
+  var orderId = complaintId % orders.length - 1;
+
+  if (orderId === 0) {
+    orderId = utils.randomIntFromInterval(1, orders.length - 1);
+  }
+  return [complaintId, reason, orderId];
+}
+
+function getComplaints() {
+  const complaintsArray = [];
+  complaintsArray.push(["id", "Przyczyna", "ID Zamówienia"]);
+  for (var i = 1; i <= COMPLAINTS_AMOUNT; i++) {
+    const complaint = randComplaint(i);
+    complaintsArray.push(complaint);
+  }
+  return complaintsArray;
+}
+
+module.exports = {
+  complaints: getComplaints(),
+  csvPath: "./generated-data/complaints.csv"
+}
--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const ordersTable = require("./fake-data-tables/orders.table");
 const mealsTable = require("./fake-data-tables/meals.table");
 const productsTable = require("./fake-data-tables/products.table");
 const preparationsTable = require("./fake-data-tables/preparations.table");
+const complaintsTable = require("./fake-data-tables/complaints.table");
 
 require("./utils").writeToCSV(restaurantsTable.restaurants, restaurantsTable.csvPath); // Restaurants
 require("./utils").writeToCSV(ordersTable.orders, ordersTable.csvPath); // Orders
 require("./utils").writeToCSV(mealsTable.meals, mealsTable.csvPath); // Meals
 require("./utils").writeToCSV(productsTable.products, productsTable.csvPath); // Products
 require("./utils").writeToCSV(preparationsTable.preparations, preparationsTable.csvPath); // Preparations
+require("./utils").writeToCSV(complaintsTable.complaints, complaintsTable.csvPath); // Complaints
