8595d6bdbc3cfc03c08e057d0cdf933113bab252
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-10-29 22:38:34 +0100

    Change sql directory

--- sql/KFC_INSERT.sql
+++ sql/KFC_INSERT.sql
@@ -1,5 +1,5 @@
 BULK INSERT ZAMOWIENIA
-FROM 'C:\Users\Karolina\Desktop\HD\mssql-data-generator\generated-data\t1\ordersT1.csv'
+FROM 'F:\Studia\SEMESTR 5\HD\dane\t1\ordersT1.csv'
 	WITH
 	(
 		FIELDTERMINATOR = ',',
@@ -11,7 +11,7 @@ select * from ZAMOWIENIA
 /*~~~~~~~~*/
 
 BULK INSERT RESTAURACJE
-FROM 'C:\Users\Karolina\Desktop\HD\mssql-data-generator\generated-data\t1\restaurantsT1.csv'
+FROM 'F:\Studia\SEMESTR 5\HD\dane\t1\restaurants.csv'
 	WITH
 	(
 		FIELDTERMINATOR = ',',
@@ -22,7 +22,7 @@ select * from RESTAURACJE
 
 /*~~~~~~~~*/
 BULK INSERT PRODUKTY
-FROM 'C:\Users\Karolina\Desktop\HD\mssql-data-generator\generated-data\t1\productsT1.csv'
+FROM 'F:\Studia\SEMESTR 5\HD\dane\t1\productsT1.csv'
 	WITH
 	(
 		FIELDTERMINATOR = ',',
@@ -35,7 +35,7 @@ select * from PRODUKTY
 
 TE NIE DZIALJA*/
 BULK INSERT ZESTAWY
-FROM 'C:\Users\Karolina\Desktop\HD\mssql-data-generator\generated-data\t1\mealsT1.csv'
+FROM 'F:\Studia\SEMESTR 5\HD\dane\t1\mealsT1.csv'
 	WITH
 	(
 		FIELDTERMINATOR = ',',
@@ -45,7 +45,7 @@ FROM 'C:\Users\Karolina\Desktop\HD\mssql-data-generator\generated-data\t1\mealsT
 select * from ZESTAWY
 /*~~~~~~~~*/
 BULK INSERT REKLAMACJE
-FROM 'C:\Users\Karolina\Desktop\HD\mssql-data-generator\generated-data\t1\complatintsT1.csv'
+FROM 'F:\Studia\SEMESTR 5\HD\dane\t1\complatintsT1.csv'
 	WITH
 	(
 		FIELDTERMINATOR = ',',
@@ -55,7 +55,7 @@ FROM 'C:\Users\Karolina\Desktop\HD\mssql-data-generator\generated-data\t1\compla
 select * from REKLAMACJE
 /*~~~~~~~~*/
 BULK INSERT PRZYGOTOWANIA
-FROM 'C:\Users\Karolina\Desktop\HD\mssql-data-generator\generated-data\t1\preparationsT1.csv'
+FROM 'F:\Studia\SEMESTR 5\HD\dane\t1\preparationsT1.csv'
 	WITH
 	(
 		FIELDTERMINATOR = ',',
